bf4e9982eec9b88a4009e330634d41b0826b5ad7
Shinelon <taylorchen@wecaiyi.com>
2016-05-27 11:10:24 +0800

    test

--- loginSSHCommand.sh
+++ loginSSHCommand.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/expect
+#!/usr/bin/expect -f
 
-spawn /usr/bin/ssh root@192.168.3.132
-expect "*password:"
+set timeout 30
+set host "192.168.3.132"
+spawn ssh $host
+expect_before "no)?" {
+send "yes\r" }
+sleep 1
+expect "password:"
 send "chenxiao\r"
-expect "*]#"
-send "ls"
-expect "*]#"
-send "exit\r"
-expect eof
+expect "*#"
+send "echo my name is fivetrees > /root/fivetrees.txt\r"
+interact
