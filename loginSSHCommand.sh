a1293de54b0b071b2af6e000dc47639c1bccb386
Shinelon <taylorchen@wecaiyi.com>
2016-05-27 14:04:57 +0800

    test

--- loginSSHCommand.sh
+++ loginSSHCommand.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/expect -f
 
 set timeout 30
-set host "192.168.3.132"
-spawn ssh root@$host
-expect_before "no)?" {
-send "yes\r" }
-sleep 1
-expect "password:"
-send "chenxiao\r"
-expect "*#"
-send "ls /root\r"
-send "echo my name is fivetrees > /root/fivetrees.txt\r"
-interact
+remoteServerHost=192.168.3.132
+sshName=root
+sshPassWord=chenxiao
+
+function stopLXC(){
+    set host "$1"
+    spawn ssh $2@$host
+    expect_before "no)?" {
+    send "yes\r" }
+    sleep 1
+    expect "password:"
+    send "$3\r"
+    expect "*#"
+    send "lxc-stop -n $4\r"
+    send "lxc-info -n $4\r"
+    send "exit\r"
+}
+
+stopLXC $remoteServerHost $sshName $sshPassWord u1
+
+
