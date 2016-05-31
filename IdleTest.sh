08b10ef6cf0cc34cc1cdf947f31da55ae488e945
Shinelon <taylorchen@wecaiyi.com>
2016-05-31 14:53:26 +0800

    test

--- IdleTest.sh
+++ IdleTest.sh
@@ -20,6 +20,8 @@ threshold=20
 #definite test lxc memory size array
 lxc_mem_array=(512 1024 1536 2048 2560 3072 3584)
 length=${#lxc_mem_array[@]}
+#definite precision
+precision_length=6
 
 #stop and copy SCLXCLM
 function SCLXCLM() {
@@ -64,25 +66,50 @@ function SCLXCLM() {
 #test SCLXCLM according to the threshold
 function test_SCLXCLM() {
     #definite real test iterations
-    num_real_iters=0
-    iters_sum=0
-    frozen_time_sum=0
-    syn_time_sum=0
-    restore_time_sum=0
-    total_time_sum=0
     for i in $lxc_mem_array
     do
+        num_real_iters=0
+        iters_sum=0
+        frozen_time_sum=0
+        syn_time_sum=0
+        restore_time_sum=0
+        total_time_sum=0
         echo "test memory size="${lxc_mem_array[$i]}
         for j in {1..$threshold}
         do
             result_tmp=""
             SCLXCLM ${lxc_mem_array[$i]}"M"
-            echo ""
+            echo "******iter"$j":result="$result_tmp
+            if [ ${#result_tmp} -gt 8 ]; then
+                iter_tmp=`echo $result_tmp | cut -d" " -f1`
+                iters_sum=`expr $iters_sum + $iter_tmp`
+                frozen_time_tmp=`echo $result_tmp | cut -d" " -f2`
+                frozen_time_sum=`expr $frozen_time_sum + $frozen_time_tmp`
+                syn_time_tmp=`echo $result_tmp | cut -d" " -f3`
+                syn_time_sum=`expr $syn_time_sum + $syn_time_tmp`
+                restore_time_tmp=`echo $result_tmp | cut -d" " -f4`
+                restore_time_sum=`expr $restore_time_sum + $restore_time_tmp`
+                total_time_tmp=`echo $result_tmp | cut -d" " -f5`
+                total_time_sum=`expr $total_time_sum + $total_time_tmp`
+                #update iters
+                num_real_iters=`expr $num_real_iters+1`
+            else
+                continue
+            fi
         done
+        #caluate the result
+        real_result=$(awk 'BEGIN{printf "%.0f\n",'$iters_sum'/'$num_real_iters'}')
+        real_result=$real_result" "$(awk 'BEGIN{printf "%.'$precision_length'f\n",'$frozen_time_sum'/'$num_real_iters'}')
+        real_result=$real_result" "$(awk 'BEGIN{printf "%.'$precision_length'f\n",'$syn_time_sum'/'$num_real_iters'}')
+        real_result=$real_result" "$(awk 'BEGIN{printf "%.'$precision_length'f\n",'$restore_time_sum'/'$num_real_iters'}')
+        real_result=$real_result" "$(awk 'BEGIN{printf "%.'$precision_length'f\n",'$total_time_sum'/'$num_real_iters'}')
+        echo "*******lxc_mem_size="${lxc_mem_array[$i]}"M,result is:"$real_result
+        echo ${lxc_mem_array[$i]}"M "$real_result >> $result_file_path
     done
 
 }
 
-result_1024M=""
-SCLXCLM 1024M
-echo "******"$result_1024M
+test_SCLXCLM
+#result_1024M=""
+#SCLXCLM 1024M
+#echo "******"$result_1024M
