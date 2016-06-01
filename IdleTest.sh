9f7633b28a6723c93a69469dd4d1f2bcb88a735f
Shinelon <taylorchen@wecaiyi.com>
2016-06-01 20:03:07 +0800

    test

--- IdleTest.sh
+++ IdleTest.sh
@@ -104,7 +104,7 @@ function test_SCLXCLM() {
             else
                 continue
             fi
-            sleep 30
+            sleep 60
         done
         #caluate the result
         real_result=$(awk 'BEGIN{printf "%.0f\n",'$iters_sum'/'$num_real_iters'}')
@@ -114,7 +114,7 @@ function test_SCLXCLM() {
         real_result=$real_result" "$(awk 'BEGIN{printf "%.'$precision_length'f\n",'$total_time_sum'/'$num_real_iters'}')
         echo "*******lxc_mem_size="$i"M,result is:"$real_result
         echo $i"M "$real_result >> $result_file_path
-        sleep 30
+        sleep 60
     done
 
 }
